39b48f02907439a82f9ba025b3c08c473c629f09
Ivayla66 <ilie0018@hz.nl>
2025-07-03 21:56:27 +0200

    added docker environment

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@ Route::get('/', function () {
 Route::get('/index', [IndexController::class, 'index']);
 Route::get('/horoscope',[HoroscopeController::class, 'horoscope']);
 Route::get('/rizz_counter_graph', [RizzCounterController::class, 'rizzCounter']);
+Route::view('/year1', 'year1');
+Route::view('/year2', 'year2');
+Route::view('/year3', 'year3');
+Route::view('/year4', 'year4');
